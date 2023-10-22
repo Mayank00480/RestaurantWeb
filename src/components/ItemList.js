6e034c48d1df8b887dfc56e7ef3fe337a05f09c8
Mayank00480 <mayanksharma1937@gmail.com>
2023-10-22 15:43:30 +0530

    Added +ADD button

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,8 +5,12 @@ const ItemList = (props) => {
     <div className = 'itemList'>
       {
         props.data.map((item) =>{
-           return <> <li key ={Math.random().toString()}>{item.itemName} <br/> {item.itemDescription}<br/> {item.itemPrice} </li>
-            <hr/></>
+           return <> <li key ={Math.random().toString()}>{item.itemName} <br/> {item.itemDescription}<br/> {item.itemPrice} </li> <form>
+            <label>Amount</label> <input type = "text"/>
+            <br/>
+            <button type = "button" >+Add</button>
+           </form>
+            <hr style={{width : '500px'}}/></>
         })
       }
     </div>
