cf775e7cda143aa090582875becfa351ada34d72
Mayank00480 <mayanksharma1937@gmail.com>
2023-10-22 12:18:35 +0530

    Added Summary

---
+++ src/components/Summary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Summary.css' 
+const Summary = () => {
+  return (
+    <div className = "summary">
+      <h1>Delicious Food, Delivered To You</h1>
+      
+      <p> Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>
+      
+      <p>All our meals are cooked with high quality ingredients, just-in-time and of course by experience chefs!</p>
+    </div>
+  )
+}
+
+export default Summary
